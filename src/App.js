8dfe2ea04fa6e3b3b704df096e7002c2b9251010
Paul Ly <hwanderlustdev@gmail.com>
2020-05-05 13:51:07 -0400

    allow absolute paths for imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './styles/App.css';
-import "./styles/Header.css";
+import 'styles/App.css';
+import "styles/Header.css";
 
 function App() {
   return (
