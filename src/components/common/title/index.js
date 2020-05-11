9624d3fcf605230a352af4f69a3ae1ebf21a43d0
Paul Ly <hwanderlustdev@gmail.com>
2020-05-11 13:37:03 -0400

    use text transform to capitalize words instead

--- src/components/common/title/index.js
+++ src/components/common/title/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { capitalizeEach } from "helpers";
 import "./Title.css";
 
 function Title(props) {
   return (
     <div className="title__container">
-      <h1 className="title__text">{capitalizeEach(props.text)}</h1>
+      <h1 className="title__text">{props.text}</h1>
       <div className="title__line"></div>
     </div>
   )
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,12 +2,4 @@ function isInternalPath(url) {
   return url.substr(0, 1) === "/";
 }
 
-function capitalize(str) {
-  return str.substr(0, 1).toUpperCase() + str.substr(1);
-}
-
-function capitalizeEach(str) {
-  return str.split(" ").map(capitalize).join(" ");
-}
-
-export { capitalize, capitalizeEach, isInternalPath };
+export { isInternalPath };
