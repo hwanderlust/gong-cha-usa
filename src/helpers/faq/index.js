b0e0335401d5d358cffb1ba930b9d2b89b805e85
Paul Ly <hwanderlustdev@gmail.com>
2020-05-14 14:32:35 -0400

    refactor sectionsPropType

--- src/helpers/faq/index.js
+++ src/helpers/faq/index.js
@@ -1,36 +1,8 @@
 import PropTypes from "prop-types";
 
-const sectionsPropType = PropTypes.arrayOf(
-  function (value, key, component, location, propName) {
-    /* Enforce the following:
-      1. value is an array
-      2. each element is an object with a 'group': string and items: []
-    */
-
-    if (!Array.isArray(value)) {
-      return new Error("The 'sections' prop should be an array");
-    }
-
-    if (value.forEach) {
-
-      for (const obj of value) {
-        const keys = Object.keys(obj);
-
-        if (!keys.includes("group") || !keys.includes("items")) {
-          return new Error("The 'sections' prop's array should have a 'group' and 'items' properties");
-        }
-
-        if (typeof obj.group !== "string") {
-          return new Error("The 'group' property should be a string");
-        }
-
-        if (!Array.isArray(obj.items)) {
-          return new Error("The 'items' property should be an array")
-        }
-      }
-    }
-
-    return null;
-  }).isRequired
+const sectionsPropType = PropTypes.arrayOf(PropTypes.exact({
+  group: PropTypes.string.isRequired,
+  items: PropTypes.array.isRequired // TODO: update when FAQ page is worked on
+}));
 
 export { sectionsPropType }
